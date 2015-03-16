c88a837b5da6a8881a90f2f31099d45b33f2ecca
hoanlongsu92 <hoanlongsu92@yahoo.com>
2015-03-16 23:13:58 +0700

    first commit

---
+++ src/com/lvts/appmanager/LocationFound.java
@@ -0,0 +1,21 @@
+package com.lvts.appmanager;
+
+import com.lvts.appmanager.R;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LocationFound  extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_location_found);
+		
+		// get the action bar
+		ActionBar actionBar = getActionBar();
+		
+		// Enabling Back navigation on Action Bar icon
+		actionBar.setDisplayHomeAsUpEnabled(true);
+	}
+}
---
+++ src/com/lvts/appmanager/MainActivity.java
@@ -0,0 +1,183 @@
+package com.lvts.appmanager;
+
+import com.lvts.appmanager.adapter.TabsPagerAdapter;
+
+import com.lvts.appmanager.R;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.view.MenuInflater;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.SearchManager;
+import android.widget.SearchView;
+import android.content.Context;
+import android.os.AsyncTask;
+
+public class MainActivity extends FragmentActivity implements
+		ActionBar.TabListener {
+
+	private ViewPager viewPager;
+	private TabsPagerAdapter mAdapter;
+	private ActionBar actionBar;
+	private MenuItem refreshMenuItem;
+	
+	// Tab titles
+	private String[] tabs = { "Top Rated", "Games", "Movies" };
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// Initilization
+		viewPager = (ViewPager) findViewById(R.id.pager);
+		actionBar = getActionBar();
+		mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+
+		viewPager.setAdapter(mAdapter);
+//		actionBar.setHomeButtonEnabled(false);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);		
+		// Enabling Back navigation on Action Bar icon
+//		actionBar.setDisplayHomeAsUpEnabled(true);
+
+		// Adding Tabs
+		for (String tab_name : tabs) {
+			actionBar.addTab(actionBar.newTab().setText(tab_name)
+					.setTabListener(this));
+		}
+
+		/**
+		 * on swiping the viewpager make respective tab selected
+		 * */
+		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+			@Override
+			public void onPageSelected(int position) {
+				// on changing the page
+				// make respected tab selected
+				actionBar.setSelectedNavigationItem(position);
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+			}
+		});
+	}
+	
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main_actions, menu);
+        
+     // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search)
+                .getActionView();
+        searchView.setSearchableInfo(searchManager
+                .getSearchableInfo(getComponentName()));
+ 
+        return super.onCreateOptionsMenu(menu);
+    }
+	
+	
+	/**
+     * Async task to load the data from server
+     * **/
+    private class SyncData extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            // set the progress bar view
+            refreshMenuItem.setActionView(R.layout.action_progressbar);
+ 
+            refreshMenuItem.expandActionView();
+        }
+ 
+        @Override
+        protected String doInBackground(String... params) {
+            // not making real request in this demo
+            // for now we use a timer to wait for sometime
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+ 
+        @Override
+        protected void onPostExecute(String result) {
+            refreshMenuItem.collapseActionView();
+            // remove the progress bar view
+            refreshMenuItem.setActionView(null);
+        }
+    };
+    
+	/**
+     * On selecting action bar icons
+     * */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Take appropriate action for each action item click
+        switch (item.getItemId()) {
+        case R.id.action_search:
+            // search action
+            return true;
+        case R.id.action_refresh:
+        	// refresh
+            refreshMenuItem = item;
+            // load the data from server
+            new SyncData().execute();
+            return true;            
+        case R.id.action_location_found:
+            // location found
+            LocationFound();
+            return true;
+        case R.id.action_help:
+            // help action
+            return true;
+        case R.id.action_check_updates:
+            // check for updates action
+            return true;
+        
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+    
+    /**
+     * Launching new activity
+     * */
+    private void LocationFound() {
+//    	Toast.makeText(getApplicationContext(), "Data saved", Toast.LENGTH_LONG).show();
+    	
+        Intent i = new Intent(MainActivity.this, LocationFound.class);
+        startActivity(i);
+    }
+    
+    
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// on tab selected
+		// show respected fragment view
+		viewPager.setCurrentItem(tab.getPosition());
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+	}
+
+}
