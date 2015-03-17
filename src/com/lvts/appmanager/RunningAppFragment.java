2fb2ea41f989b89da6afa729ec4d1c17b0c71d4a
hoanlongsu92 <hoanlongsu92@yahoo.com>
2015-03-17 15:49:56 +0700

    add listview on Fragment but get a lot of errors

--- src/com/lvts/appmanager/RunningAppFragment.java
+++ src/com/lvts/appmanager/RunningAppFragment.java
@@ -7,14 +7,118 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.lvts.appmanager.adapter.CustomListAdapter;
+import com.lvts.appmanager.app.AppController;
+import com.lvts.appmanager.model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.ListView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+
 public class RunningAppFragment extends Fragment {
-
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-
+	// Log tag
+	private static final String TAG = MainActivity.class.getSimpleName();
+	
+	// Movies json url
+    private static final String url = "http://api.androidhive.info/json/movies.json";
+    private ProgressDialog pDialog;
+    private List<Movie> movieList = new ArrayList<Movie>();
+    private ListView listView;
+    private CustomListAdapter adapter;
+    
+    @Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		
+		listView = (ListView) findViewById(R.id.list);
+        adapter = new CustomListAdapter(this, movieList);
+        listView.setAdapter(adapter);
+        
+     // Creating volley request obj
+        JsonArrayRequest movieReq = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+                        hidePDialog();
+ 
+                        // Parsing json
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+ 
+                                JSONObject obj = response.getJSONObject(i);
+                                Movie movie = new Movie();
+                                movie.setTitle(obj.getString("title"));
+                                movie.setThumbnailUrl(obj.getString("image"));
+                                movie.setRating(((Number) obj.get("rating"))
+                                        .doubleValue());
+                                movie.setYear(obj.getInt("releaseYear"));
+ 
+                                // Genre is json array
+                                JSONArray genreArry = obj.getJSONArray("genre");
+                                ArrayList<String> genre = new ArrayList<String>();
+                                for (int j = 0; j < genreArry.length(); j++) {
+                                    genre.add((String) genreArry.get(j));
+                                }
+                                movie.setGenre(genre);
+ 
+                                // adding movie to movies array
+                                movieList.add(movie);
+ 
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+ 
+                        }
+ 
+                        // notifying list adapter about data changes
+                        // so that it renders the list view with updated data
+                        adapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        VolleyLog.d(TAG, "Error: " + error.getMessage());
+                        hidePDialog();
+ 
+                    }
+                });
+ 
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(movieReq);      
 		View rootView = inflater.inflate(R.layout.fragment_running_app, container, false);
 		
 		return rootView;
 	}
+
+
+	@Override
+	public void onDestroy() {
+	    super.onDestroy();
+	    hidePDialog();
+	}
+	
+	private void hidePDialog() {
+	    if (pDialog != null) {
+	        pDialog.dismiss();
+	        pDialog = null;
+	    }
+	}
+
 }
